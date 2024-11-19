15a12a75f6e851a9bcf8e53b8a03f085b67331ba
Huan Zhou <huanzhou2006@gmail.com>
2024-11-19 22:20:06 +0800

    solve part one of day 19

--- aoc2018/19.cpp
+++ aoc2018/19.cpp
@@ -4,8 +4,8 @@ using namespace aoc;
 class ElfDevice {
 public:
     using VecInt = std::vector<int>;
+    using Program = std::vector<VecInt>;
     using Op = void(*)(const VecInt&, VecInt&);
-    using Program = std::vector<std::string>;
 
     static void addr(const VecInt& code, VecInt& regs) { regs[code[3]] = regs[code[1]] + regs[code[2]]; }
     static void addi(const VecInt& code, VecInt& regs) { regs[code[3]] = regs[code[1]] + code[2]; }
@@ -24,14 +24,14 @@ public:
     static void eqri(const VecInt& code, VecInt& regs) { regs[code[3]] = regs[code[1]] == code[2] ? 1 : 0; }
     static void eqrr(const VecInt& code, VecInt& regs) { regs[code[3]] = regs[code[1]] == regs[code[2]] ? 1 : 0; }
 
-    static std::map<std::string, Op> ops;
+    const static std::vector<Op> ops;
+    const static std::map<std::string, int> name_index;
 
 public:
     ElfDevice() : m_regs(6) {}
-    void run(const Program& program);
-
-private:
-    void instruct(const std::string& inst);
+    void bind_ip(int n) { m_bind = n; }
+    void run(const Program& program, int r0, int debug = 1);
+    int& reg(int r) { return m_regs[r]; }
 
 private:
     int m_ip = 0;
@@ -39,30 +39,68 @@ private:
     VecInt m_regs;
 };
 
-std::map<std::string, ElfDevice::Op> ElfDevice::ops {
-    {"addr", addr}, {"addi", addi},
-    {"mulr", mulr}, {"muli", muli},
-    {"banr", banr}, {"bani", bani},
-    {"borr", borr}, {"bori", bori},
-    {"setr", setr}, {"seti", seti},
-    {"gtir", gtir}, {"gtri", gtri}, {"gtrr", gtrr},
-    {"eqir", eqir}, {"eqri", eqri}, {"eqrr", eqrr},
+const std::vector<ElfDevice::Op> ElfDevice::ops {
+    ElfDevice::addr, ElfDevice::addi,
+    ElfDevice::mulr, ElfDevice::muli,
+    ElfDevice::banr, ElfDevice::bani,
+    ElfDevice::borr, ElfDevice::bori,
+    ElfDevice::setr, ElfDevice::seti,
+    ElfDevice::gtir, ElfDevice::gtri, ElfDevice::gtrr,
+    ElfDevice::eqir, ElfDevice::eqri, ElfDevice::eqrr,
+};
+const std::map<std::string, int> ElfDevice::name_index {
+    {"addr", 0}, {"addi", 1},
+    {"mulr", 2}, {"muli", 3},
+    {"banr", 4}, {"bani", 5},
+    {"borr", 6}, {"bori", 7},
+    {"setr", 8}, {"seti", 9},
+    {"gtir", 10}, {"gtri", 11}, {"gtrr", 12},
+    {"eqir", 13}, {"eqri", 13}, {"eqrr", 15},
 };
 
-static int as_digit(char c) { return c - '0'; }
-
-void ElfDevice::run(const Program& program) {
+void ElfDevice::run(const Program& program, int r0, int debug) {
     std::fill(m_regs.begin(), m_regs.end(), 0);
-    m_bind = -1;
-    for (m_ip = 0; )
-        if (line[0] == '#') {
-            m_bind = as_digit(line[4]);
-        } else {
-            VecInt code(4);
-            VecInt tmp = ints(line);
-            for (int i = 0; i < 3; ++i) code[i+1] = tmp[i];
-            auto opstr = line.substr(0, 4);
-            ops[opstr](code, m_regs);
-        }
+    m_regs[0] = r0;
+
+    for (m_ip = 0; 0 <= m_ip && m_ip < (int)program.size(); ++m_ip) {
+        m_regs[m_bind] = m_ip;
+        if (debug >= 2) std::cout << "[ED] " << m_ip << ' ' << m_regs << ": " << program[m_ip];
+        ops[program[m_ip][0]](program[m_ip], m_regs);
+        if (debug >= 2) std::cout << " => " << m_regs << std::endl;
+        m_ip = m_regs[m_bind];
     }
+    if (debug >= 1) std::cout << "[ED] HALT ip = " << m_ip << std::endl;
+}
+
+void parse_input(int& bind, ElfDevice::Program& program) {
+    std::string line;
+    std::getline(std::cin, line);
+    bind = line[4] - '0';
+
+    while (std::getline(std::cin, line)) {
+        std::vector<int> inst(4);
+        inst[0] = ElfDevice::name_index.at(line.substr(0, 4));
+        auto tmp = ints(line);
+        for (size_t i = 0; i < 3; ++i) inst[i+1] = tmp[i];
+        program.push_back(inst);
+    }
+
+    std::cout << "bind = " << bind << std::endl;
+    // for (const auto& v : program) std::cout << v << std::endl;
+    std::cout << "program size = " <<  program.size() << std::endl;
+}
+
+int solve_one(int bind, ElfDevice::Program program, int r0 = 0) {
+    ElfDevice dev;
+    dev.bind_ip(bind);
+    dev.run(program, r0);
+    return dev.reg(0);
+}
+
+int main() {
+    int bind;
+    ElfDevice::Program program;
+    parse_input(bind, program);
+    print_answer("one", solve_one(bind, program));
+    print_answer("two", solve_one(bind, program, 1));
 }
