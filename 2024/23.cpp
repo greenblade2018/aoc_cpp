b9cd4483e6b1c98e5aa934a55bcd02e37bbc6937
Huan Zhou <huanzhou2006@gmail.com>
2024-12-23 19:32:24 +0800

    solve part two of day 23 from 2024

--- 2024/23.cpp
+++ 2024/23.cpp
@@ -10,6 +10,36 @@ string calc_sig(string u, string v, string w) {
     return tmp[0] + "," + tmp[1] + "," + tmp[2];
 }
 
+set<string> intersect(const set<string>& a, const set<string>& b) {
+    set<string> c;
+    for (const auto& x : a) {
+        if (b.count(x)) c.insert(x);
+    }
+    return c;
+}
+
+set<string> clique;
+
+void BK(set<string> R, set<string> P, set<string> X) {
+    if (P.empty() && X.empty()) {
+        cout << "Maximal clique: " << R << endl;
+        if (R.size() > clique.size()) clique = R;
+        return;
+    }
+
+    for (auto it = P.begin(); it != P.end(); /*NONE*/) {
+        auto v = *it;
+        auto NR = R;
+        NR.insert(v);
+        auto NP = intersect(P, graph[v]);
+        auto NX = intersect(X, graph[v]);
+        BK(NR, NP, NX);
+
+        it = P.erase(it);
+        X.insert(v);
+    }
+}
+
 int main() {
     string line;
     while(getline(cin, line)) {
@@ -32,4 +62,15 @@ int main() {
     int one = count_if(result.cbegin(), result.cend(),
                        [](const string& s) { return s[0] == 't' || s.find(",t") != string::npos; });
     print_answer("one", one);
+
+    set<string> all;
+    for (const auto& p : graph) all.insert(p.first);
+    cout << "all: " << all << endl;
+    BK(set<string>(), all, set<string>());
+    cout << "part two:\n";
+    for (auto it = clique.begin(); it != clique.end(); ++it) {
+        if (it != clique.begin()) cout << ',';
+        cout << *it;
+    }
+    cout << endl;
 }
